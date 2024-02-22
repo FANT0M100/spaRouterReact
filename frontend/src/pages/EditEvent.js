5afa9937df8506f0f8d54c1e45cff692c7753998
FANT0M100 <giorgie103188@gmail.com>
2024-02-22 18:05:09 +0400

    add new evemt

--- frontend/src/pages/EditEvent.js
+++ frontend/src/pages/EditEvent.js
@@ -1,9 +1,13 @@
+import { useRouteLoaderData } from "react-router-dom";
 import EventForm from "../components/EventForm";
 
 const EditEventPage = () => {
+  const data = useRouteLoaderData("event-detail");
+  const event = data.event;
+
   return (
     <>
-      <EventForm />
+      <EventForm event={event} />
     </>
   );
 };
--- frontend/src/pages/NewEvent.js
+++ frontend/src/pages/NewEvent.js
@@ -1,5 +1,11 @@
+import EventForm from "../components/EventForm";
+
 const NewEventPage = () => {
-  return <h1>New Event Page</h1>;
+  return (
+    <>
+      <EventForm />
+    </>
+  );
 };
 
 export default NewEventPage;
